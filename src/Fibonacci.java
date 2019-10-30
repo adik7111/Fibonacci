9843361722ebcc53e1ba245dbfdc24cc59ba8967
K1631110 <adrian.kucharski@kcl.ac.uk>
2019-10-30 13:41:55 +0000

    Add recursiveFib function which returns term provided.

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,6 +1,21 @@
 public class Fibonacci {
 
+    /*
+    Following the fibonacci pattern,
+    F0 = 0
+    F1 = 1
+    Fn = Fn-1 + Fn - 2
+    Calls back recursively onto method making time complexity O(2^n).
+     */
+    public static int recursiveFib(int term) {
+        if (term <= 0) return 0;
+        if (term == 1) return 1;
+
+        return recursiveFib(term - 1) + recursiveFib(term - 2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Set Up");
+        //expected output is 55
+        System.out.println(recursiveFib(10));
     }
 }
