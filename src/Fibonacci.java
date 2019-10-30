5626980214fc6f9032d703ae800c0ce5ae069eee
K1631110 <adrian.kucharski@kcl.ac.uk>
2019-10-30 13:46:55 +0000

    Add linearFib function which returns term provided.
    Runs in linear time in comparison to recursive running in exponential time

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -14,8 +14,27 @@ public class Fibonacci {
         return recursiveFib(term - 1) + recursiveFib(term - 2);
     }
 
+    /*
+    Similarly to above, but stores the two previous terms to add
+    meaning it continues linearly (Will run term - 1 time) = O(n);
+     */
+    public static int linearFib(int term) {
+        int first = 0;
+        int second = 1;
+        int current = 1;
+
+        for (int i = 1; i < term; i++) {
+            current = first + second;
+            first = second;
+            second = current;
+        }
+
+        return current;
+    }
+
     public static void main(String[] args) {
         //expected output is 55
         System.out.println(recursiveFib(10));
+        System.out.println(linearFib(10));
     }
 }
